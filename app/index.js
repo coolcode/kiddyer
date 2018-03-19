6d93961b43c48f50b68ce8a6dd264d04ce4bcbf0
Bruce Lee <coolcode@live.com>
2018-03-20 00:00:31 +1100

    FetchExample

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import MyWeb from './pages/MyWeb';
+import FetchExample from './pages/FetchExample';
 
-AppRegistry.registerComponent('app', () => App);
+AppRegistry.registerComponent('app', () => FetchExample);
