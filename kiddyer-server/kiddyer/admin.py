8c66ebcabfb090dbbeaa444e3001b9fe64455fba
Alex Wu <wutian@vlan-2618-10-16-76-163.student.wireless.sydney.edu.au>
2018-03-20 10:51:42 +1100

    models

--- kiddyer-server/kiddyer/admin.py
+++ kiddyer-server/kiddyer/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
 
 # Register your models here.
+from kiddyer import models
+
+class AuthGroupAdmin(admin.ModelAdmin):
+    list_display = ["id", "__str__"]
+
+    class Meta:
+        model = models.AuthGroup
+
+
+class AuthUserGroupAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "group_id"]
+
+    class Meta:
+        model = models.AuthUserGroup
+
+class AuthGroupPermissionAdmin(admin.ModelAdmin):
+    list_display = ["group_id", "permission_id"]
+
+    class Meta:
+        model = models.AuthGroupPermission
+
+
+class AppMsgAdmin(admin.ModelAdmin):
+    list_display = ["sender", "receiver", "content", "type", "attachment", "create_Date"]
+
+    class Meta:
+        model = models.AppMsg
+
+
+class AuthPermissionAdmin(admin.ModelAdmin):
+    list_display = ["name", "content_type_id", "code_name"]
+
+    class Meta:
+        model = models.AuthPermission
+
+class AuthUserUserPermissionAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "permission_id"]
+
+    class Meta:
+        model = models.AuthUserUserPermission
+
+
+class AuthUserAdmin(admin.ModelAdmin):
+    list_display = ["email", "password", "user_name", "first_name", "last_name", "last_login", "is_superuser", "is_staff", "is_active", "date_joined"]
+
+    class Meta:
+        model = models.AuthUser
+
+
+
+class AppUserGroupAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "group_id", "create_Date"]
+
+    class Meta:
+        model = models.AppUserGroup
+
+
+
+class AppUserProfileAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "icon", "mobile_num", "create_Date"]
+
+    class Meta:
+        model = models.AppUserProfile
+
+
+
+
+class AppGroupAdmin(admin.ModelAdmin):
+    list_display = ["name", "user_id", "create_Date"]
+
+    class Meta:
+        model = models.AppGroup
+
+
+
+
+admin.site.register(models.AuthGroup, AuthGroupAdmin)
+admin.site.register(models.AuthUserGroup, AuthUserGroupAdmin)
+admin.site.register(models.AuthGroupPermission, AuthGroupPermissionAdmin)
+admin.site.register(models.AppMsg, AppMsgAdmin)
+admin.site.register(models.AuthPermission, AuthPermissionAdmin)
+admin.site.register(models.AuthUserUserPermission, AuthUserUserPermissionAdmin)
+admin.site.register(models.AuthUser, AuthUserAdmin)
+admin.site.register(models.AppUserGroup, AppUserGroupAdmin)
+admin.site.register(models.AppUserProfile, AppUserProfileAdmin)
+admin.site.register(models.AppGroup, AppGroupAdmin)
--- kiddyer-server/kiddyer/models.py
+++ kiddyer-server/kiddyer/models.py
@@ -1,3 +1,99 @@
 from django.db import models
 
 # Create your models here.
+class AuthGroup(models.Model):
+    name = models.CharField(max_length=80, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class AuthUserGroup(models.Model):
+    user_id = models.IntegerField()
+    group_id = models.IntegerField()
+
+    def __str__(self):
+        return str(self.user_id)
+
+
+
+class AuthGroupPermission(models.Model):
+    group_id = models.IntegerField()
+    permission_id = models.IntegerField()
+
+    def __str__(self):
+        return str(self.group_id)
+
+
+class AppMsg(models.Model):
+    sender = models.IntegerField()
+    receiver = models.IntegerField()
+    content = models.CharField(max_length=1024)
+    type = models.CharField(max_length=255)
+    attachment = models.CharField(max_length=1024)
+    create_Date = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return str(self.sender)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type_id = models.IntegerField()
+    code_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class AuthUserUserPermission(models.Model):
+    user_id = models.IntegerField()
+    permission_id = models.IntegerField()
+
+    def __str__(self):
+        return str(self.user_id)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(auto_now_add=False, auto_now=True)
+    is_superuser = models.BooleanField()
+    user_name = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.email)
+
+class AppUserGroup(models.Model):
+    user_id = models.IntegerField()
+    group_id = models.IntegerField()
+    create_Date = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return str(self.user_id)
+
+
+class AppUserProfile(models.Model):
+    user_id = models.IntegerField()
+    icon = models.CharField(max_length=4096)
+    mobile_num =models.CharField(max_length=20)
+    create_Date = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return str(self.user_id)
+
+
+class AppGroup(models.Model):
+    name = models.CharField(max_length=255)
+    user_id = models.IntegerField()
+    create_Date = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return str(self.user_id)
