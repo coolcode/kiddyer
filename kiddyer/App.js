848a661bbc4bdf05a70e5401e5939080938474ef
Bruce Lee <coolcode@live.com>
2018-03-21 10:28:37 +1100

    native gmaps

--- kiddyer/App.js
+++ kiddyer/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import {Actions, Router, Stack, Scene} from 'react-native-router-flux';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import WebViewExample from './pages/WebViewExample';
-import FetchExample from './pages/FetchExample';
-
+import Login from './components/Login';
+import Register from './components/Register';
+import WebViewExample from './components/WebViewExample';
+import FetchExample from './components/FetchExample';
+import Gmap from './components/Gmap';
+//gmap: AIzaSyDuAKhfwvK2xPn7qJTdQiHNZqirWZpgvGQ
 export default class App extends React.Component {
   
   constructor(props) {
@@ -14,18 +15,18 @@ export default class App extends React.Component {
     this.state = { loading: true };
   }
 
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-    });
-    this.setState({ loading: false });
-  }
+  // async componentWillMount() {
+  //   await Expo.Font.loadAsync({
+  //     Roboto: require("native-base/Fonts/Roboto.ttf"),
+  //     Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+  //     Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+  //   });
+  //   this.setState({ loading: false });
+  // }
 
   render() {
     if (this.state.loading) {
-      return <Expo.AppLoading />;
+      // return <Expo.AppLoading />;
     }
     return (
       <Router>
@@ -33,10 +34,10 @@ export default class App extends React.Component {
           <Scene key="login"
             component={Login}
             title="Login"
-            rightTitle="Register"
-            onRight={() => Actions.register()}
+            rightTitle="Gmap"
+            onRight={() => Actions.gmap()}
             />
-          <Scene key="register" component={Register} title="Register"
+          <Scene key="gmap" component={Gmap} title="Gamp"
               rightTitle="WebView"
               onRight={() => Actions.webview()} />
           <Scene key="webview" component={WebViewExample} title="WebView" 
---
+++ kiddyer/components/Gmap.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Button, Text, Form, Item, Input } from 'native-base';
+import MapView from 'react-native-maps';
+
+export default class Gmap extends Component { 
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      } 
+    };
+  }
+  
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+  
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+  
+  render() {
+    return (
+      <MapView
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
+      />
+    );
+  }
+   
+}
