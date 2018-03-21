e3f1dbd9ec4c367793c17d9338aec220930e4f91
Will <yliu0522@uni.sydney.edu.au>
2018-03-21 15:42:12 +1100

    project structure and navigation

---
+++ kiddyer/src/components/Gmap.js
@@ -0,0 +1,68 @@
+import React,{ Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Container, Header, Content, Button, Text, Form, Item, Input } from 'native-base';
+import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default class Gmap extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    };
+  }
+
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    const { region } = this.props;
+    console.log(region);
+
+    return (
+      //<View style ={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        </MapView>
+     // </View>
+    );
+  }
+
+}
--- kiddyer/src/components/Register.js
+++ kiddyer/src/components/Register.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { ScrollView, Text, TextInput, View, Button } from 'react-native';
 
 export default class Register extends Component {
-    render () {
+    render() {
         return (
             <View><Text>Register Page</Text></View>
         );
