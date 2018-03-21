9bae13faac8a23aaa8645453fd3bcf29903146d4
Bruce Lee <coolcode@live.com>
2018-03-21 21:09:42 +1100

    merge

--- kiddyer/index.js
+++ kiddyer/index.js
@@ -1,10 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
-<<<<<<< HEAD
-// import Gmap from './components/Gmap';
-
-AppRegistry.registerComponent('kiddyer', () => App);
-=======
+import App from './App'; 
 import Gmap from './src/components/Gmap';
-AppRegistry.registerComponent('kiddyer', () => Gmap);
->>>>>>> 5cd3fd9852d3c8889e1b8fc157decfe9f4f5b5c0
+
+AppRegistry.registerComponent('kiddyer', () => Gmap); 
