07c8f053dd4b841244ae8907276a2943527a7730
Bruce Lee <coolcode@live.com>
2018-03-22 11:50:44 +1100

    pubnub

--- kiddyer/src/components/PubNub.js
+++ kiddyer/src/components/PubNub.js
@@ -11,9 +11,10 @@ import PubNub from 'pubnub'
 
 export default class ReactPubNub extends Component {
   render() {
+    console.log("PubNub Demo");
     return (
         <View>
-            <Text>{this.state}</Text>
+            <Text>PubNub</Text>
         </View>
     );
   }
@@ -36,3 +37,29 @@ pubnub.subscribe({
     channels: ['map-channel'] 
 });
 
+
+function randomMove(loc) {
+    let r = Math.random();
+    if (r < 0.3) {
+        loc.lat += 0.0001;
+        loc.lng -= 0.0001;
+    } else if (r < 0.5) {
+        loc.lat -= 0.0003;
+        loc.lng += 0.0001;
+    } else if (r < 0.7) {
+        loc.lat -= 0.00005;
+        loc.lng -= 0.0002;
+    } else {
+        loc.lat += 0.00005;
+        loc.lng += 0.0003;
+    }
+    return loc;
+}
+
+let currentLocation = {lat:-33.8655721, lng: 151.2048194};  
+
+setTimeout(function(){      
+    currentLocation = randomMove(currentLocation);
+    pubnub.publish({channel: 'map-channel', message: currentLocation});
+    console.log("realtime location: " + currentLocation.lat + ","+ currentLocation.lng);
+}, 2000);
