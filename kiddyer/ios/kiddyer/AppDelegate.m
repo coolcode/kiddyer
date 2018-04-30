9cfc8a0a720fa448079dec56427adbe5063e7c50
Will <yliu0522@uni.sydney.edu.au>
2018-04-30 12:48:58 +1000

    backup

--- kiddyer/ios/kiddyer/AppDelegate.m
+++ kiddyer/ios/kiddyer/AppDelegate.m
@@ -12,11 +12,14 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import GoogleMaps; //add this line if you want to use Google Maps
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
+
+  [GMSServices provideAPIKey:@"AIzaSyAaWc0BWylp9rkZMXlqBab1gQwjTAv168w"];
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
